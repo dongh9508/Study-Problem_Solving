afa83c886c7d34feddd5ee5d87ffb8e038fe4751
dongh9508 <odh621@naver.com>
2018-09-25 16:21:27 +0900

    edit code t_18.java

--- week1/week1_실습코드/t_18.java
+++ week1/week1_실습코드/t_18.java
@@ -5,12 +5,11 @@ import java.util.*;
 public class t_18 {
     public static void main(String[] args) {
         int n[]={5,7,9,1,3,5,8,2,4,9};
-        int y = n.length-1;
         Arrays.sort(n);
-        for(int i=0; i<n.length; i++) {
-            int x = n[y];
-            n[i] = x;
-            y--;
+        for(int i=0; i<n.length/2; i++) {
+            int x = n[i];
+            n[i] = n[n.length-i-1];
+            n[n.length-i-1] = x;
         }
         System.out.println(Arrays.toString(n));
     }
